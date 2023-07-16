9b19d59ea893e44e08faa190a4f7779ead279956
Rachit4039 <122766495+Rachit4039@users.noreply.github.com>
2023-07-16 14:59:49 +0530

    Update Prime_or_not.cpp

--- day2/Prime_or_not.cpp
+++ day2/Prime_or_not.cpp
@@ -8,9 +8,11 @@ int main(){
     while(i<n){
         if(n%2==0){
             cout<<"Not Prime";
+            break;
         }
         else{
             cout<<"Prime";
+            break;
         }
     i=i+1;
     }
