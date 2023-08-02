444ee1a4c970a0ea9fc62a255b8ee96aaab7ddfe
Rachit4039 <122766495+Rachit4039@users.noreply.github.com>
2023-08-02 17:37:53 +0530

    Add files via upload

---
+++ day 10/selectionsort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i;
+    int j;
+    int n;
+    int arr[n];
+    cin>>n;
+    for (i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    for (i=0;i<n-1;i++){
+
+
+        for (j=i+1;j<n;j++){
+            if (arr[j]<arr[i]){
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+
+            }
+        }
+        
+    }
+    for (i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
---
+++ day 10/sqrt.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//     int i;
+//     int n;
+//     cin>>n;
+//     for(i=1;i<n;i++){
+//         if (i*i==n){
+//             cout<<"the square root of "<<n<<" is " <<i;
+//         }
+//         else{
+            
+//         }
+//     }
+// }
+
+#include <math.h>
+  
+int main () 
+{
+    float x, result;
+    cout<<"Enter a number "<<endl;
+    cin>>x;
+
+    
+    result = sqrt(x);
+    cout<<"Square root "<< x << " is "<<result;
+    
+
+}
